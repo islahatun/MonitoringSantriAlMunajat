8b8a42a0975009ee3cf7a356c325504e0e8744a0
islahatun <islahatunnufusi98@gmail.com>
2022-05-25 22:37:33 +0700

    Create Menu Kelas

---
+++ application/controllers/Kelas.php
@@ -0,0 +1,93 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Kelas extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('MFunction');
+		$this->load->model('MKelas');
+	}
+
+	public function index()
+	{
+		$data['title'] = 'Kelas';
+		$data['subtitle'] = 'Data Kelas';
+
+		$data['content_overview'] = $this->load->view('kelas', $data, true);
+		$this->load->view('overview', $data);
+	}
+
+	public function datalist()
+	{
+		$list = $this->MKelas->datalist();
+
+		$rtn = array();
+		$i = 1;
+
+		foreach ($list as $kelas) {
+
+			$rtn[] = array(
+				'nomor'         		=> $i,
+				'id_kelas'         => $kelas->id_kelas,
+				'nama_kelas'       => $kelas->nama_kelas,
+				'btn_action'            => "<a href='" . base_url('Kelas/update/' . $kelas->id_kelas) . "' class='btn btn-sm btn-outline-success'> 
+												<i class='fas fa-edit'></i>
+											</a>
+											<button type='button' id='btn_kleas_del' vid_kelas=" . $kelas->id_kelas . " class='btn btn-sm btn-outline-danger'> 
+												<i class='fas fa-trash-alt'></i>
+											</button>"
+			);
+			$i++;
+		}
+
+		echo json_encode($rtn);
+	}
+
+	public function add()
+	{
+		$data['title'] = 'Kelas';
+		$data['subtitle'] = 'Tambah Kelas';
+
+		$data['content_overview'] = $this->load->view('kelas/formadd', $data, true);
+		$this->load->view('overview', $data);
+	}
+
+	public function update($id)
+	{
+		$data['title'] = 'Kelas';
+		$data['subtitle'] = 'Ubah Kelas';
+		$data["kelas"] = $this->MKelas->getById($id);
+
+		$data['content_overview'] = $this->load->view('kelas/formupdate', $data, true);
+		$this->load->view('overview', $data);
+	}
+
+	public function save()
+	{
+		$data = array(
+			'nama_kelas' => $this->input->post('nama_kelas'),
+		);
+
+		$this->db->insert("kelas", $data);
+	}
+
+	function updateSave()
+	{
+		$data = array(
+			'nama_kelas' => $this->input->post('nama_kelas'),
+		);
+
+		$this->db->where('id_kelas', $this->input->post('id_kelas'));
+		$this->db->update("kelas", $data);
+	}
+
+	public function delete()
+	{
+
+
+		$this->db->where('id_kelas', $this->input->post('id_kelas'));
+		$this->db->delete("dm_kelas");
+	}
+}
