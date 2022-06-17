8ae4cc397151fd969a5845fac096db18d5810b7c
islahatun <islahatunnufusi98@gmail.com>
2022-06-17 23:44:28 +0700

    create hapalan role guru

---
+++ application/controllers/HapalanRoleGuru.php
@@ -0,0 +1,108 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class HapalanRoleGuru extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('MFunction');
+        $this->load->model('MHapalanRoleGuru');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Hapalan';
+        $data['subtitle'] = 'Data Hapalan';
+
+        $data['content_overview'] = $this->load->view('HapalanRoleGuru', $data, true);
+        $this->load->view('overview', $data);
+    }
+
+    public function datalist()
+    {
+        $list = $this->MHapalanRoleGuru->datalist();
+
+        $rtn = array();
+        $i = 1;
+
+        foreach ($list as $ao) {
+
+            $rtn[] = array(
+                'nomor'               => $i,
+                'nisn'         => $ao->nisn,
+                'nama_santri'    => $ao->nama_santri,
+                'juz'    => $ao->juz,
+                'surah'    => $ao->surah,
+                'ayat'    => $ao->ayat,
+                'tanggal'    => $ao->tanggal,
+                'btn_action'         => "<a href='" . base_url('HapalanRoleGuru/update/' . $ao->id_hapalan) . "' class='btn btn-sm btn-outline-success'> 
+												<i class='fas fa-edit'></i>
+											</a>
+											<button type='button' id='btn_id_hapalan_del' vid_hapalan=" . $ao->id_hapalan . " class='btn btn-sm btn-outline-danger'> 
+												<i class='fas fa-trash-alt'></i>
+											</button>"
+            );
+            $i++;
+        }
+
+        echo json_encode($rtn);
+    }
+
+    public function add()
+    {
+        $data['title'] = 'Hapalan';
+        $data['subtitle'] = 'Tambah Hapalan';
+
+        $data['content_overview'] = $this->load->view('HapalanRoleGuru/formadd', $data, true);
+        $this->load->view('overview', $data);
+    }
+
+    public function update($id)
+    {
+        $data['title'] = 'Hapalan';
+        $data['subtitle'] = 'Ubah Hapalan';
+        $data["ao"] = $this->MHapalanRoleGuru->getById($id);
+
+        $data['content_overview'] = $this->load->view('HapalanRoleGuru/formupdate', $data, true);
+        $this->load->view('overview', $data);
+    }
+
+    public function save()
+    {
+        $data = array(
+            'nisn' => $this->input->post('nisn'),
+            'nama_santri' => $this->input->post('nama_santri'),
+            'juz' => $this->input->post('juz'),
+            'surah' => $this->input->post('surah'),
+            'ayat' => $this->input->post('ayat'),
+            'tanggal' => $this->input->post('tanggal'),
+        );
+
+        $this->db->insert("dm_hapalan", $data);
+        return ('Satuan');
+    }
+
+    function updateSave()
+    {
+        $data = array(
+            'nisn' => $this->input->post('nisn'),
+            'nama_santri' => $this->input->post('nama_santri'),
+            'juz' => $this->input->post('juz'),
+            'surah' => $this->input->post('surah'),
+            'ayat' => $this->input->post('ayat'),
+            'tanggal' => $this->input->post('tanggal'),
+        );
+
+        $this->db->where('id_hapalan', $this->input->post('id_hapalan'));
+        $this->db->update("dm_hapalan", $data);
+    }
+
+    public function delete()
+    {
+
+
+        $this->db->where('id_hapalan', $this->input->post('id_hapalan'));
+        $this->db->delete("dm_hapalan");
+    }
+}
---
+++ application/views/HapalanRoleGuru/formadd.php
@@ -0,0 +1,165 @@
+<!-- SweetAlert2 -->
+<link rel="stylesheet" href="<?= base_url('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css'); ?>">
+<!-- Toastr -->
+<link rel="stylesheet" href="<?= base_url('assets/plugins/toastr/toastr.min.css'); ?>">
+<!-- Select2 -->
+<link rel="stylesheet" href="<?= base_url('assets/plugins/select2/css/select2.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css'); ?>">
+<!-- DataTables -->
+<link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css'); ?>">
+
+<div class="content-wrapper" style="min-height: 1302.12px;">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1><?= $title; ?></h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title"><?= $subtitle; ?></h3>
+                        </div>
+                        <form id="form_poliklinik" class="form-horizontal">
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-2 col-form-label-sm">NISN</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control form-control-sm" id="nisn" placeholder="NISN">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-2 col-form-label-sm">Nama Santri</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control form-control-sm" id="nama_santri" placeholder="Nama Santri">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-2 col-form-label-sm">Juz</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control form-control-sm" id="juz" placeholder="Juz">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-2 col-form-label-sm">Surah</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control form-control-sm" id="surah" placeholder="Surah">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-2 col-form-label-sm">Ayat</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control form-control-sm" id="ayat" placeholder="Ayat">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="" class="col-sm-2 col-form-label-sm">Tanggal</label>
+                                    <div class="col-sm-10">
+                                        <input type="date" class="form-control form-control-sm" id="tanggal" value="<?= date('Y-m-d') ?>" placeholder="Tanggal">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer text-right">
+                                <a href="<?= base_url('HapalanRoleGuru'); ?>" type="button" class="btn btn-secondary">Kembali</a>
+                                <button type="button" class="btn btn-primary" onclick="savejuz()">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+
+
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+<!-- Page specific script -->
+<script>
+    // $(function() {
+    //     $('#nama_santri').select2({
+    //         theme: 'bootstrap4'
+    //     });
+    // });
+
+    function savejuz() {
+        //debugger
+        PatchURL = _baseurl.concat('/HapalanRoleGuru/save');
+
+        var vnisn = $("#nisn").val();
+        var vnama_santri = $("#nama_santri").val();
+        var vjuz = $("#juz").val();
+        var vsurah = $("#surah").val();
+        var vayat = $("#ayat").val();
+        var vtanggal = $("#tanggal").val();
+
+        var value = {
+            nisn: vnisn,
+            nama_santri: vnama_santri,
+            juz: vjuz,
+            surah: vsurah,
+            ayat: vayat,
+            tanggal: vtanggal
+        };
+
+        $.ajax({
+            type: "POST",
+            url: PatchURL,
+            data: value,
+            cache: false,
+            success: function(data, textStatus, jqXHR) {
+                debugger
+                //var data = jQuery.parseJSON(data);
+                toastr.success('Data berhasil disimpan.');
+
+                clearText();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                toastr.error('Data gagal disimpan.');
+            }
+        });
+    }
+
+
+    function clearText() {
+        $("#nisn").val("");
+        $("#nama_santri").val("");
+        $("#ayat").val("");
+        $("#juz").val("");
+        $("#surah").val("");
+    }
+</script>
+<!-- SweetAlert2 -->
+<script src="<?= base_url('assets/plugins/sweetalert2/sweetalert2.min.js'); ?>"></script>
+<!-- Toastr -->
+<script src="<?= base_url('assets/plugins/toastr/toastr.min.js'); ?>"></script>
+<!-- DataTables  & Plugins -->
+<script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/jszip/jszip.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/pdfmake/pdfmake.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/pdfmake/vfs_fonts.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-buttons/js/buttons.html5.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-buttons/js/buttons.print.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-buttons/js/buttons.colVis.min.js'); ?>"></script>
