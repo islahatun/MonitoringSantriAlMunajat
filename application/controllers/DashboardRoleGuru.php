177abf989591d656b03711f003b55fb0d854370c
islahatun <islahatunnufusi98@gmail.com>
2022-06-29 20:40:58 +0700

    read pengurus

--- application/controllers/DashboardRoleGuru.php
+++ application/controllers/DashboardRoleGuru.php
@@ -15,4 +15,30 @@ class DashboardRoleGuru extends CI_Controller
 		$data['content_overview'] = $this->load->view('DashboardRoleGuru', $data, true);
 		$this->load->view('overview', $data);
 	}
+
+	public function pengurus()
+	{
+		$list = $this->MDashboard->pengurus();
+
+		$rtn = array();
+		$i = 1;
+
+		foreach ($list as $Pengurus) {
+
+			$rtn[] = array(
+				'nomor'         		=> $i,
+				'nama_pengurus'         => $Pengurus->nama_pengurus,
+				'jabatan'       => $Pengurus->jabatan,
+				// 'btn_action'            => "<a href='" . base_url('Pengurus/update/' . $Pengurus->id_pengurus) . "' class='btn btn-sm btn-outline-success'> 
+				// 								<i class='fas fa-edit'></i>
+				// 							</a>
+				// 							<button type='button' id='btn_Pengurus_del' vid_pengurus=" . $Pengurus->id_pengurus . " class='btn btn-sm btn-outline-danger'> 
+				// 								<i class='fas fa-trash-alt'></i>
+				// 							</button>"
+			);
+			$i++;
+		}
+
+		echo json_encode($rtn);
+	}
 }
